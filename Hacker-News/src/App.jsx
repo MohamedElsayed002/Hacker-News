456638053accae0d7073f39e2e1b84e4f4022e69
Mohamed Elsayed Ali <mohamedelsayed20258@gmail.com>
2023-05-10 16:24:45 +0300

    Hacker News

---
+++ Hacker-News/src/App.jsx
@@ -0,0 +1,17 @@
+
+import SearchForm from "./SearchForm"
+import Stories from "./Stories"
+import Buttons from "./Buttons"
+
+const App = () => {
+  return (
+    <>
+      <SearchForm/>
+      <Buttons/>
+
+      <Stories/>
+    </>
+  )
+}
+
+export default App
